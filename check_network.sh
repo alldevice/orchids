5e6853b1789f95c1fa07383f5255cb00dddfb8a8
Alldevice <39749056+alldevice@users.noreply.github.com>
2018-05-31 16:52:22 +0300

    Update check_network.sh

--- check_network.sh
+++ check_network.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+OUT_IP_1=google.com
+	#OUT_IP_1=$(route | grep default | awk '{print $2}') # с таким вариантом ВСЁ помирает 
+x=`ping -c1 $OUT_IP_1 2>&1 | grep -e unknown -e Unreachable -e '0 received'`
+if [ ! "$x" = "" ]; then
+        echo "It's down!! Attempting to restart."
+        service networking restart
+        echo "network restarted $(date)" >> /home/pi/log_restart_network.log
+fi
+
+
+OUT_IP_2=25.86.120.215 # adress for check
+x=`ping -c1 $OUT_IP_2 2>&1 | grep -e unknown -e Unreachable -e '0 received'`
+if [ ! "$x" = "" ]; then
+                sudo /etc/init.d/logmein-hamachi restart
+                echo "hamachi restarted $(date)" >> /home/pi/log_restart_hamachi.log
+fi
